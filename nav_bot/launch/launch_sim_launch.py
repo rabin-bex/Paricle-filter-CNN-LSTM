cf4f4aca3aafaa17e44ce483553ed11bdbcf2f9f
Rabin Giri <75846015+rabin-bex@users.noreply.github.com>
2023-12-24 02:01:52 +0545

    Add files via upload
    robot discription and launch pkg added

---
+++ nav_bot/launch/launch_sim_launch.py
@@ -0,0 +1,73 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+
+
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+
+    package_name='nav_bot' #<--- CHANGE ME
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control':'false'}.items()
+    )
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+             )
+
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'robot',
+                                #    '-world','usr/share/gazebo-11/worlds/cafe.world'
+                                #     '-world', '/home/rabin/ros2/nav_ws/src/nav_bot/worlds/obstacles.world',
+                                   ],
+                        output='screen')
+    
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        # parameters=[my_controllers],
+        # output="both",
+    )
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
+
+
+    # Launch them all!
+    return LaunchDescription([
+        rsp,
+        # control_node,
+        gazebo,
+        spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner,
+    ])
